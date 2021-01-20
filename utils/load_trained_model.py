f4fe23a3f1d9fdce394402937862cf5296fb84a3
Mustafa Mustafa <mstftsm@gmail.com>
2021-01-19 20:31:53 -0800

    Refactor and create a method to load directly from checkpoint

--- utils/load_trained_model.py
+++ utils/load_trained_model.py
@@ -32,30 +32,35 @@ def load_experiment(yaml_config_file='./config/photoz.yaml', config='default', l
   params.log()
   params['log_to_screen'] = True
 
-  return load(params, load_best_ckpt, device), params
-
-def load(params, load_best_ckpt, device=torch.cuda.current_device()):
-
-  model = models.resnet.resnet50(num_channels=params.num_channels, num_classes=params.num_classes).to(device)
-
-  ckpt_file = params.checkpoint_path
+  checkpoint_path = params.checkpoint_path
   if load_best_ckpt:
-    ckpt_file = ckpt_file.replace('.tar', '_best.tar')
-  logging.info("Loading checkpoint %s"%ckpt_file)
-  restore_checkpoint(model, ckpt_file)
+    checkpoint_path = checkpoint_path.replace('.tar', '_best.tar')
+
+  model = load_model_from_checkpoint(checkpoint_path, params.num_channels, num_classes=params.num_classes, device=device)
+
+  return model, params
+
+def load_model_from_checkpoint(checkpoint_path, num_channels=5, num_classes=180, device=torch.cuda.current_device()):
+  model = models.resnet.resnet50(num_channels=num_channels, num_classes=num_classes).to(device)
+
+  logging.info("Loading checkpoint %s"%checkpoint_path)
+  restore_checkpoint(model, checkpoint_path)
 
   return model
 
 def restore_checkpoint(model, checkpoint_path):
   checkpoint = torch.load(checkpoint_path, map_location='cuda:0')
 
+  # some checkpoints have a different name for the model key
+  model_key = 'model_state' if 'model_state' in checkpoint else 'state_dict'
+
   new_model_state = OrderedDict()
-  for key in checkpoint['model_state'].keys():
+  for key in checkpoint[model_key].keys():
     if 'module.' in key:
       name = str(key).replace('module.', '')
-      new_model_state[name] = checkpoint['model_state'][key]
+      new_model_state[name] = checkpoint[model_key][key]
     else:
-      new_model_state[key] = checkpoint['model_state'][key]
+      new_model_state[key] = checkpoint[model_key][key]
 
   model.load_state_dict(new_model_state)
   logging.info("Chekpoint loaded. Checkpoint epoch %d"%checkpoint['epoch'])
